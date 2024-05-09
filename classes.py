5e6aac6a23b6a2f0825a0bf5ec2a59c5418ba04d
Samir M. <samirmohd246@gmail.com>
2024-05-08 20:15:27 -0400

    Add main function and begin work on actual game logic

--- classes.py
+++ classes.py
@@ -6,30 +6,47 @@ class Card:
         self.suit = suit
 
     def __str__(self):
-            value_names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
-            # Use the value_names dictionary to convert numerical values to names, if applicable
-            # If the value is not in the dictionary (2-10), it will just use the number
-            value_str = value_names.get(self.value, str(self.value))
-            return f"{value_str} of {self.suit}"
+        value_names = {1: "Ace", 11: "Jack", 12: "Queen", 13: "King"}
+        # Use the value_names dictionary to convert numerical values to names, if applicable
+        # If the value is not in the dictionary (2-10), it will just use the number
+        value_str = value_names.get(self.value, str(self.value))
+        return f"{value_str} of {self.suit}"
 
-suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-suits_symbol = ["♥︎","♦︎","♣︎","♠︎"]
+def shuffle(deck):
+    random.shuffle(deck)
 
-deck = [Card(value, suit) for value in range(1, 14) for suit in suits_symbol] # interchange suits_symbol and suits
+def printDeck(deck):
+    for card in deck:
+        print(card)
 
-for card in deck:
-     print(card)
+def checkBust(hand):
+    if (sum(hand) > 21):
+        return True
 
-playerShuffle = True
+def checkBlackjack(hand):
+    if (sum(hand) = 21):
+        return True
 
-while(playerShuffle):
-    checkShuffle = str(input("Type 'shuffle' to shuffle the deck \n"))
+def blackJack(deck):
+    while (len(deck) > 8):
+        dealerHand = []
+        playerHand = []
 
-    if (checkShuffle == "shuffle"):
-        print("============ Shuffled deck ============")
-        random.shuffle(deck)
-        for card in deck:
-            print(card)
-    else:
-        playerShuffle = False
+        dealerHand.append
 
+def main():
+    playerShuffle = True
+
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    suits_symbol = ["♥︎","♦︎","♣︎","♠︎"]
+    deck = [Card(value, suit) for value in range(1, 14) for suit in suits_symbol] # interchange suits_symbol and suits
+
+    shoeSize = int(input("How many decks of cards do you want in the shoe? \n"))
+    deck = deck * shoeSize
+    shuffle(deck)
+
+    blackJack(deck)
+
+    printDeck()
+
+main()
