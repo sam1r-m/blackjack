b39f603524b1014c4379f8ff415379c20c4f3680
Samir M. <samirmohd246@gmail.com>
2024-05-08 20:26:52 -0400

    Complete printHand functions and beginning of blackJack function

--- classes.py
+++ classes.py
@@ -19,25 +19,42 @@ def printDeck(deck):
     for card in deck:
         print(card)
 
+def printHand(hand):
+    for card in hand:
+        print(card)
+
 def checkBust(hand):
     if (sum(hand) > 21):
         return True
 
 def checkBlackjack(hand):
-    if (sum(hand) = 21):
+    if (sum(hand) == 21):
         return True
 
 def blackJack(deck):
     while (len(deck) > 8):
-        dealerHand = []
-        playerHand = []
+        dealerHand = ["Dealer Hand:"]
+        playerHand = ["Player Hand:"]
 
-        dealerHand.append
+        for i in range (0, 2):
+            playerHand.append(deck.pop())
+            dealerHand.append(deck.pop())
+        
+        printHand(dealerHand)
+        print("\n")
+        printHand(playerHand)
+        print("\n")
+        #print("Dealer Hand: Unknown, " + dealerHand[1] + "\n")
+        #print("Player Hand: " + playerHand[0] + " " + playerHand[1] + "\n")
+
+        deck = ""
+        
+
 
 def main():
     playerShuffle = True
 
-    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"] # Replaced by suits_symbol for clarity
     suits_symbol = ["♥︎","♦︎","♣︎","♠︎"]
     deck = [Card(value, suit) for value in range(1, 14) for suit in suits_symbol] # interchange suits_symbol and suits
 
@@ -46,7 +63,5 @@ def main():
     shuffle(deck)
 
     blackJack(deck)
-
-    printDeck()
 
 main()
